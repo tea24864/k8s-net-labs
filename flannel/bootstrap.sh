5619b196afea5de4092e8f65866ba2832942619b
tea24864 <timchung1@gmail.com>
2023-08-12 12:57:17 -0500

    Update bootstrap.sh
    Added "env INSTALL_K3S_SELINUX_WARN=true" to avoid failing installing k3s as a work-around so that we can proceed with the network lab.
    
    Added an option to cp proxy ca trust bundle if we are running this lab behind a (corporate) proxy.

--- flannel/bootstrap.sh
+++ flannel/bootstrap.sh
@@ -7,13 +7,20 @@ docker pull korvus/debian10:0.6.3
 
 footloose create
 
+# Copy the trust bundle, if exists, and configure docker for Docker/k3s to work behind a proxy
+if [[ -f tls-ca-bundle.pem ]]
+  docker cp tls-ca-bundle.pem footloose-k8s-node0:/etc/ssl/certs/
+  docker cp tls-ca-bundle.pem footloose-k8s-node1:/etc/ssl/certs/
+  docker cp tls-ca-bundle.pem footloose-k8s-node2:/etc/ssl/certs/
+fi
+  
 # set up k3s on node0 as the master
-footloose ssh root@node0 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true /root/install-k3s.sh"
+footloose ssh root@node0 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env INSTALL_K3S_SELINUX_WARN=true /root/install-k3s.sh"
 
 # get the token from node0
 export k3stoken=$(footloose ssh root@node0 -- cat /var/lib/rancher/k3s/server/node-token)
 
 # set up k3s on node1 and node2 with the token from node0
-footloose ssh root@node1 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
-footloose ssh root@node2 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken /root/install-k3s.sh"
+footloose ssh root@node1 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken env INSTALL_K3S_SELINUX_WARN=true /root/install-k3s.sh"
+footloose ssh root@node2 -- "env INSTALL_K3S_SKIP_DOWNLOAD=true env K3S_URL=https://node0:6443 env K3S_TOKEN=$k3stoken env INSTALL_K3S_SELINUX_WARN=true /root/install-k3s.sh"
 
